e8d5b5255a0440718aa6cca85d2bbffcf523433b
Justin Tervay <justin@tervay.com>
2024-11-16 17:32:06 -0500

    Add search_index API route

--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -38,6 +38,7 @@ from backend.api.handlers.helpers.profiled_jsonify import profiled_jsonify
 from backend.api.handlers.insights import insights_leaderboards_year
 from backend.api.handlers.match import match, zebra_motionworks
 from backend.api.handlers.media import media_tags
+from backend.api.handlers.search import search_index
 from backend.api.handlers.status import status
 from backend.api.handlers.team import (
     team,
@@ -277,6 +278,9 @@ api_v3.add_url_rule(
 api_v3.add_url_rule(
     "/insights/leaderboards/<int:year>", view_func=insights_leaderboards_year
 )
+
+# Search
+api_v3.add_url_rule("/search_index", view_func=search_index)
 
 # Trusted API
 trusted_api = Blueprint("trusted_api", __name__, url_prefix="/api/trusted/v1")
