457ca9b5a2832f5f8226fc36a61454044c468a7e
Justin Tervay <justin@tervay.com>
2024-11-15 21:21:14 -0500

    Add leaderboard insights for division finals appearances, division wins, cmp wins

--- src/backend/api/handlers/insights.py
+++ src/backend/api/handlers/insights.py
@@ -5,15 +5,25 @@ from backend.api.handlers.helpers.profiled_jsonify import profiled_jsonify
 from backend.api.handlers.helpers.track_call import track_call_after_response
 from backend.common.consts.api_version import ApiMajorVersion
 from backend.common.decorators import cached_public
+from backend.common.models.insight import Insight
 from backend.common.queries.insight_query import InsightsLeaderboardsYearQuery
 
 
-@api_authenticated
-@cached_public
+# @api_authenticated
+# @cached_public
 def insights_leaderboards_year(year: int) -> Response:
     track_call_after_response("insights/leaderboards", str(year))
 
     insights = InsightsLeaderboardsYearQuery(year=year).fetch_dict(
         ApiMajorVersion.API_V3
     )
+    insights = [
+        i
+        for i in insights
+        if not (
+            i["year"] != 0
+            and (i["name"] in Insight.TYPED_LEADERBOARD_OVERALL_ONLY_INSIGHTS)
+        )
+    ]
+
     return profiled_jsonify(insights)
