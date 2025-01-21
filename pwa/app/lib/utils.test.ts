704d98acf5e1c60f9eca6f35e7ff400154f16b32
Justin Tervay <justin@tervay.com>
2025-01-21 18:18:24 -0500

    [PWA] Add COPRs and basic match stats to event insights tab

--- pwa/app/lib/utils.test.ts
+++ pwa/app/lib/utils.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest';
 
-import { removeNonNumeric, slugify } from './utils';
+import { median, removeNonNumeric, slugify } from './utils';
 
 describe.concurrent('removeNonNumeric', () => {
   test('basic', () => {
@@ -14,5 +14,13 @@ describe.concurrent('slugify', () => {
     expect(slugify('FIRST Championship - Houston')).toEqual(
       'first-championship-houston',
     );
+  });
+});
+
+describe.concurrent('median', () => {
+  test('basic', () => {
+    expect(median([1, 2, 3, 4, 5])).toEqual(3);
+    expect(median([1, 2, 3, 4, 5, 6])).toEqual(3.5);
+    expect(median([1, 2, 3, 4, 5, 6, 7])).toEqual(4);
   });
 });
