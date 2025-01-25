e14da72f7b850e5f8595dc46f06c567875c0264e
Justin Tervay <justin@tervay.com>
2025-01-24 19:38:21 -0500

    [PWA] Add COPRs and basic match stats to event insights tab

--- pwa/app/lib/utils.tsx
+++ pwa/app/lib/utils.tsx
@@ -127,3 +127,14 @@ export function joinComponents(
     </React.Fragment>
   ));
 }
+
+// https://stackoverflow.com/a/70806192
+export function median(arr: number[]): number | undefined {
+  if (!arr.length) {
+    return undefined;
+  }
+
+  const s = [...arr].sort((a, b) => a - b);
+  const mid = Math.floor(s.length / 2);
+  return s.length % 2 ? s[mid] : (s[mid - 1] + s[mid]) / 2;
+}
